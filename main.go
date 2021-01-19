d1545e3a463b2f3f4fdc898511382749233d824c
KEINOS <github@keinos.com>
2021-01-19 11:05:42 +0900

    docs: Add comments to ease reading

--- main.go
+++ main.go
@@ -9,12 +9,13 @@ import (
 	"github.com/KEINOS/Hello-Cobra/cmd"
 )
 
+// Preparation to ease the test of "exit status" check.
 // https://stackoverflow.com/a/40801733/8367711
 var osExit = os.Exit
 
 func main() {
-	// `cmd.Execute()` initializes Cobra such as adding sub-commands to the root
-	// command and executes the designated command.
+	// `cmd.Execute()` initializes the app such as adding sub-commands to the root
+	// command and then executes the designated command.
 	// If `cmd.Execute()` returns an error then it will print the error message
 	// to STDERR and exits with 1.
 	osExit(cmd.EchoIfError(cmd.Execute()))
