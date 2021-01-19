2dcc9bc1349681f76641b6a884ab0532989b1ad8
KEINOS <github@keinos.com>
2021-01-19 12:28:23 +0900

    Update flag.go

--- cmd/flag.go
+++ cmd/flag.go
@@ -4,32 +4,79 @@ Package cmd flag is a sub-command that detects flags and it's value from the arg
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
+
+/*
+-------------------------------------------------------------------------------
+ Define global variables.
+
+ These variables may be defined as local. But mostly option/flag arguments are
+ referenced through out the application, so we set here as global to let the
+ other packages to reference them.
+-------------------------------------------------------------------------------
+*/
+
+// Alpha stores the given value of the "--alpha" "-a" flag option. Default: empty
+var Alpha string
+
+// Beta stores the bool state. Default: false, with the '--beta' flag: true.
+var Beta bool
+
+/*
+-------------------------------------------------------------------------------
+ Define local variables
+-------------------------------------------------------------------------------
+*/
 
 // flagCmd represents the flag command
 var flagCmd = &cobra.Command{
 	Use:   "flag",
-	Short: "Displays the value of the flag.",
+	Short: "Displays the internal value according to the flags provided.",
 	Long: `About:
-	This command detects the flag in the argument and outputs it's value.
+	This command detects the flag in the argument, and outputs it's internal value.
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(cmd)
+		echoFlagStatuses(cmd, args)
 	},
 }
 
-// Alpha stores the value of the flag
-var Alpha string
+/*
+-------------------------------------------------------------------------------
+ Define local functions
+-------------------------------------------------------------------------------
+*/
 
+// echoFlagStatuses is the actual body function of flagCmd.
+func echoFlagStatuses(cmd *cobra.Command, args []string) {
+	echoValueAlpha()
+	echoValueBeta()
+}
+
+func echoValueAlpha() {
+	if Alpha == "" {
+		Echo("The value of Alpha: '' (empty. Option not provided)")
+	} else {
+		Echo("The value of Alpha: " + Alpha)
+	}
+}
+
+func echoValueBeta() {
+	if Beta {
+		Echo("The value of Beta: true")
+	} else {
+		Echo("The value of Beta: false (Option not provided)")
+	}
+}
+
+// Initialization when `cmd.Execute()` was called.
 func init() {
-	// Add flag options of this command
+	// Add flag options of this command:
+	// * The flag --alpha/-a will store it's value into "Alpha" global variable.
+	flagCmd.Flags().StringVarP(&Alpha, "alpha", "a", "", "Displays the given string of option-value. A value is required.")
+	// * The flag --beta/-b will store it's bool state into "Beta" global variable.
+	flagCmd.Flags().BoolVarP(&Beta, "beta", "b", false, "Sets the 'beta' boolean flag to 'true'. (Default: false)")
 
-	// --source/-s Stores it's value to "Alpha" global variable.
-	flagCmd.Flags().StringVarP(&Alpha, "alpha", "a", "", "Displays the given option value string.")
-
-	// Add this command to parent(rootCmd)
+	// Add this command to the parent(rootCmd)
 	rootCmd.AddCommand(flagCmd)
 }
