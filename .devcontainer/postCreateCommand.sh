f3293a2c4b2c23ef62e6d48fd02d87d527ca139b
KEINOS <github@keinos.com>
2021-01-21 20:24:44 +0900

    fix: remove flag command

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,3 +2,13 @@
 # Place any script here which you want to run after creating the container.
 
 set -eu
+
+# Sim-llink Welcome message for bash
+ln -s "$(pwd)/.devcontainer/welcome.sh" "${HOME}/.welcome.sh"
+
+# Single quotes are intensional. So, not to expand expressions.
+# shellcheck disable=SC2016
+echo '"${HOME}/.welcome.sh"' >>"${HOME}/.bashrc"
+
+# Sim-llink Cobra configuration file to home
+ln -s "$(pwd)/.devcontainer/cobra.yaml" "${HOME}/.cobra.yaml"
