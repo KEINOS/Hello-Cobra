734b6eb70e38da18184627324cd61b4f788115bf
KEINOS <github@keinos.com>
2021-11-26 13:06:40 +0900

    feat: go install test

---
+++ .github/run-tests-go-install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# =============================================================================
+#  `go install` test (Go 1.16+)
+# =============================================================================
+
+cd /tmp || {
+    echo >&2 "failed to move temp dir"
+    exit 1
+}
+
+go install "github.com/KEINOS/Hello-Cobra/hello-cobra@latest" || {
+    echo >&2 "failed to install hello-cobra command via 'go install'"
+    exit 1
+}
+
+hello-cobra --version | grep "hello-cobra version v" || {
+    echo >&2 "failed to execute command. Version info is missing"
+    exit 1
+}
